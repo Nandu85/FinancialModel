8a09915577dde74c1fede663b1c620b8b46c2cdd
Nandu85 <85878078+Nandu85@users.noreply.github.com>
2022-06-25 11:38:27 +0530

    Some Basic Change

--- src/main/java/com/narola/financialcalculator/ResultDAO.java
+++ src/main/java/com/narola/financialcalculator/ResultDAO.java
@@ -27,14 +27,14 @@ public class ResultDAO {
 		
 		result1.setExpense(financialInfo.getExpense());
 		double balance = financialInfo.getFixedAmount() + financialInfo.getIncome() - financialInfo.getExpense();
-		result1.setBalance(balance);
-		result1.setInvestment5(balance * 0.2);
-		result1.setInvestment8(balance * 0.3);
-		result1.setInvestment15(balance * 0.5);
+		result1.setBalance(round(balance));
+		result1.setInvestment5(round(balance * 0.2));
+		result1.setInvestment8(round(balance * 0.3));
+		result1.setInvestment15(round(balance * 0.5));
 		double roi = result1.getInvestment5() * 0.05 + result1.getInvestment8() * 0.08
 				+ result1.getInvestment15() * 0.15;
-		result1.setRoi(roi);
-		result1.setTotalBalance(balance + roi);
+		result1.setRoi(round(roi));
+		result1.setTotalBalance(round(balance + roi));
 
 		list.add(result1);
 
